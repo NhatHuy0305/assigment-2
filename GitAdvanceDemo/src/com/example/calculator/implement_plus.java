3d45bc8eb3e36ab0af3ec0601ea621b798f155cc
NhatHuy0305 <125416703+NhatHuy0305@users.noreply.github.com>
2025-06-29 14:14:14 +0700

    Implement PLUS calculation

---
+++ GitAdvanceDemo/src/com/example/calculator/implement_plus.java
@@ -0,0 +1,13 @@
+package com.example.calculator;
+
+public class implement_plus {
+	public Double calculate(SIGN sign, Double arg1, Double arg2) {
+        switch (sign) {
+            case PLUS:
+                return arg1 + arg2;
+            default:
+                break;
+        }
+        return 0d;
+    }
+}
